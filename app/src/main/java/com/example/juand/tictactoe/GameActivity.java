6256b040ecd9e9af40468f7bd7e3f5af7ddd38c9
JuanD0958 <juand.herrera@hotmail.com>
2018-08-25 20:29:15 -0500

    First implementation of tic tac toe game

---
+++ app/src/main/java/com/example/juand/tictactoe/GameActivity.java
@@ -0,0 +1,92 @@
+package com.example.juand.tictactoe;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.juand.tictactoe.databinding.ActivityGameBinding;
+
+public class GameActivity extends AppCompatActivity {
+
+    protected Button btnTL, btnTM, btnTR;
+    protected Button btnML, btnMM, btnMR;
+    protected Button btnBL, btnBM, btnBR;
+    protected Button restartGameButton;
+    protected TicTacToe game;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        ActivityGameBinding activityGameBinding = DataBindingUtil.setContentView(this, R.layout.activity_game);
+        setBindingWithButtons(activityGameBinding);
+        configureGameBoard();
+        setListenersToButtons();
+    }
+
+    private void configureGameBoard() {
+        game = new TicTacToe();
+        game.setController(this);
+        game.resetBoard();
+    }
+
+    private void setListenersToButtons() {
+        btnTL.setOnClickListener(event -> game.onClickButton(0, 0));
+        btnTM.setOnClickListener(event -> game.onClickButton(1, 0));
+        btnTR.setOnClickListener(event -> game.onClickButton(2, 0));
+        btnML.setOnClickListener(event -> game.onClickButton(0, 1));
+        btnMM.setOnClickListener(event -> game.onClickButton(1, 1));
+        btnMR.setOnClickListener(event -> game.onClickButton(2, 1));
+        btnBL.setOnClickListener(event -> game.onClickButton(0, 2));
+        btnBM.setOnClickListener(event -> game.onClickButton(1, 2));
+        btnBR.setOnClickListener(event -> game.onClickButton(2, 2));
+        restartGameButton.setOnClickListener(event -> game.resetBoard());
+    }
+
+    private void setBindingWithButtons(ActivityGameBinding activityGameBinding) {
+        btnTL = activityGameBinding.btnTL;
+        btnTM = activityGameBinding.btnTM;
+        btnTR = activityGameBinding.btnTR;
+        btnML = activityGameBinding.btnML;
+        btnMM = activityGameBinding.btnMM;
+        btnMR = activityGameBinding.btnMR;
+        btnBL = activityGameBinding.btnBL;
+        btnBM = activityGameBinding.btnBM;
+        btnBR = activityGameBinding.btnBR;
+        restartGameButton = activityGameBinding.restartGameButton;
+    }
+
+    public void resetButtons() {
+        String blank = "";
+        btnTL.setText(blank);
+        btnTM.setText(blank);
+        btnTR.setText(blank);
+        btnML.setText(blank);
+        btnMM.setText(blank);
+        btnMR.setText(blank);
+        btnBL.setText(blank);
+        btnBM.setText(blank);
+        btnBR.setText(blank);
+    }
+
+    public void drawPlayerOnBoard(int i, int j,String text){
+        if (i == 0 && j==0) btnTL.setText(text);
+        else if (i == 1 && j==0) btnTM.setText(text);
+        else if (i == 2 && j==0) btnTR.setText(text);
+        else if (i == 0 && j==1) btnML.setText(text);
+        else if (i == 1 && j==1) btnMM.setText(text);
+        else if (i == 2 && j==1) btnMR.setText(text);
+        else if (i == 0 && j==2) btnBL.setText(text);
+        else if (i == 1 && j==2) btnBM.setText(text);
+        else if (i == 2 && j==2) btnBR.setText(text);
+    }
+
+
+    public void showMessage(String strMessage){
+        Toast toast = Toast.makeText(this,strMessage,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
+
---
+++ app/src/main/java/com/example/juand/tictactoe/TicTacToe.java
@@ -0,0 +1,98 @@
+package com.example.juand.tictactoe;
+
+public class TicTacToe {
+    private int [][] board = {{0,0,0},{0,0,0},{0,0,0}};
+    private int playerTurn = 1;
+    private GameActivity controller;
+
+
+    public TicTacToe() {
+    }
+
+    public void setController(GameActivity controller){
+        this.controller = controller;
+    }
+
+    public int onClickButton(int i, int j){
+        if (board[i][j]!= 0) return 0;
+        checkAndDrawPlayer(i, j,1,"O");
+        checkAndDrawPlayer(i, j,2,"X");
+        checkBoardState();
+        switchPlayer();
+        return 1;
+    }
+
+    private void checkAndDrawPlayer(int i, int j, int playerOnTurn, String playerSymbol) {
+        if(board[i][j]==0 && playerTurn==playerOnTurn){
+            board[i][j] = playerTurn;
+            controller.drawPlayerOnBoard(i,j,playerSymbol);
+        }
+    }
+
+
+    private void switchPlayer(){
+        playerTurn = playerTurn^3;
+    }
+
+
+    private void checkBoardState(){
+        if (checkBoardDirections(board)){
+            showMessage("Player " + playerTurn + " has won!");
+        } else if (checkFullBoard()){
+            showMessage("Tie Game");
+        }
+    }
+
+    private void showMessage(String strMessage) {
+        controller.showMessage(strMessage);
+    }
+
+
+    private boolean checkFullBoard() {
+        for (int x=0;x<3;x++){
+            for(int y=0;y<3;y++){
+                if(board[x][y]==0) return false;
+            }
+        }
+        return true;
+    }
+
+
+    public void resetBoard() {
+        for (int x=0;x<3;x++){
+            for(int y=0;y<3;y++){
+                board[x][y]=0;
+            }
+        }
+        controller.resetButtons();
+    }
+
+    private boolean checkBoardDirections(int [][]testBoard){
+        if (checkColumns(testBoard)) return true;
+        if (checkRows(testBoard)) return true;
+        if (diagonalRight(testBoard, 0, 2)) return true;
+        if (diagonalRight(testBoard, 2, 0)) return true;
+        return false;
+    }
+
+    private boolean diagonalRight(int[][] testBoard, int i, int i2) {
+        return     testBoard[0][i] != 0
+                && testBoard[0][i] == testBoard[1][1]
+                && testBoard[1][1] == testBoard[2][i2];
+    }
+
+    private boolean checkRows(int[][] testBoard) {
+        for (int y=0;y<3;y++) {
+            return testBoard[0][y]!=0 && testBoard[0][y] == testBoard[1][y] && testBoard[1][y] == testBoard[2][y];
+        }
+        return false;
+    }
+
+    private boolean checkColumns(int[][] testBoard) {
+        for (int x=0;x<3;x++) {
+            if (testBoard[x][0]!=0 && testBoard[x][0] == testBoard[x][1] && testBoard[x][1] == testBoard[x][2])
+                return true;
+        }
+        return false;
+    }
+}
